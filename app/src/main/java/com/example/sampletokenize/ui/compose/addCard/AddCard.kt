9ea0098e40303f5a7d8d1ffdbde9e04e4e1006a1
Tariq Ebadi <tariq.ebadi@finixpayments.com>
2022-11-23 13:03:42 -0800

    moved

--- app/src/main/java/com/example/sampletokenize/ui/compose/addCard/AddCard.kt
+++ app/src/main/java/com/example/sampletokenize/ui/compose/addCard/AddCard.kt
@@ -1,5 +1,6 @@
 package com.example.sampletokenize.ui.compose
 
+import android.widget.Toast
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.BorderStroke
@@ -10,17 +11,21 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finixpaymentsheet.ui.viewModel.PaymentSheetWithResults
 import com.example.sampletokenize.R
+import com.example.sampletokenize.ui.compose.addCard.AddCardViewModel
 import com.example.sampletokenize.ui.compose.common.HorizontalPartialDivider
 import com.example.sampletokenize.ui.theme.*
 
@@ -77,10 +82,13 @@ fun DaphneysCornerPreview() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AddCardButton(modifier: Modifier = Modifier) {
+fun AddCardButton(
+    modifier: Modifier = Modifier,
+    onClick: (Boolean) -> Unit
+) {
     Card(
         onClick = {
-
+                  onClick(true)
         },
         elevation = CardDefaults.cardElevation(6.dp),
         modifier = modifier
@@ -102,55 +110,37 @@ fun AddCardButton(modifier: Modifier = Modifier) {
 }
 
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun AddCardButton2(modifier: Modifier = Modifier) {
-    Card(
-        onClick = {},
-        elevation = CardDefaults.cardElevation(6.dp),
-        modifier = modifier
-            .width(296.dp)
-            .height(170.dp),
-        shape = RoundedCornerShape(20.dp),
-        border = BorderStroke(1.dp,Color.LightGray),
-
-        ) {
-//        Icon(
-//            imageVector = Icons.Default.Add,
-//            contentDescription = null,
-//            modifier = modifier
-//                .background(lightGray)
-//                .padding(60.dp)
-//        )
-//        Image(
-//            painter = painterResource(id = R.drawable.ic_add),
-//            contentDescription = null,
-//            modifier = modifier
-//                .background(lightGray)
-//                .padding(70.dp)
-//        )
-    }
-
-}
-
-
-@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
-@Composable
-fun AddCardButtonPreview(){
-    AddCardButton()
-}
-
-@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
-@Composable
-fun AddCardButton2Preview(){
-    AddCardButton2()
-}
-
-
 @Composable
 fun AddCardScreen(
     modifier: Modifier = Modifier
 ) {
+
+    //we can also use viewModel for state
+    val viewModel = viewModel<AddCardViewModel>()
+    val state = viewModel.state
+
+    var showFinixPaymentSheet by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+
+    if (showFinixPaymentSheet) {
+        PaymentSheetWithResults(
+            onDismiss = {
+                showFinixPaymentSheet = !showFinixPaymentSheet
+                Toast.makeText(context, "Dialog dismissed!", Toast.LENGTH_SHORT).show()
+            },
+            onNegativeClick = {
+                showFinixPaymentSheet = !showFinixPaymentSheet
+                Toast.makeText(context, "Negative Button Clicked!", Toast.LENGTH_SHORT).show()
+
+            },
+            onPositiveClick = { Token ->
+                showFinixPaymentSheet = !showFinixPaymentSheet
+                Toast.makeText(context, "Tokenize Response: $Token", Toast.LENGTH_SHORT).show()
+            }
+        )
+    }
+
+
     Box(modifier = modifier, contentAlignment = Alignment.BottomCenter){
         Banner()
     }
@@ -172,14 +162,16 @@ fun AddCardScreen(
             .padding(top = 372.dp)
     ) {
         HorizontalPartialDivider(widthDP = 321.dp)
-        AddCardButton()
+        AddCardButton(onClick = { clicked ->
+            showFinixPaymentSheet = clicked
+        })
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 @Composable
-fun IconPreview() {
+fun MyAppHome() {
     SampleTokenizeTheme() {
         Scaffold { paddingValues ->
         AddCardScreen(Modifier.padding(paddingValues))
