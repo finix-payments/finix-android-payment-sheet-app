4a725ec467759ab0dd801eaf7a0b66735dd86d8e
Tariq Ebadi <tariq.ebadi@finixpayments.com>
2023-01-09 13:26:21 -0800

    added local maven finix library and test implementation finix library

--- app/src/main/java/com/example/sampletokenize/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/sampletokenize/ui/activity/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.example.sampletokenize.ui.compose.MyAppHome
+import com.example.sampletokenize.ui.compose.addCard.MyAppHome
 import com.example.sampletokenize.ui.theme.SampleTokenizeTheme
 
 class MainActivity : ComponentActivity() {
